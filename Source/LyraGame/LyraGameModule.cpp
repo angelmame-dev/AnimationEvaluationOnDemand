e96ed3fa7173f3dad25ee3ba6fb389908dbf974a
angelmame-dev <angelmmkudt@gmail.com>
2026-06-15 11:14:41 +0200

    Added and loaded lag compensation module

--- Source/LyraGame/LyraGameModule.cpp
+++ Source/LyraGame/LyraGameModule.cpp
@@ -10,6 +10,8 @@ class FLyraGameModule : public FDefaultGameModuleImpl
 {
 	virtual void StartupModule() override
 	{
+		// #todo: this will be potentially being replaced by a auto-load impementing a plugin
+		FModuleManager::Get().LoadModuleChecked<IModuleInterface>(TEXT("LagCompensation"));
 	}
 
 	virtual void ShutdownModule() override
